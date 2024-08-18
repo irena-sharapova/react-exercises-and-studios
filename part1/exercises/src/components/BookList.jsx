802ad1bd209abfa5e73f92fd43f5268cd7b65280
Irina Sharapova <ir.ur.smirnova@gmail.com>
2024-08-17 22:07:46 -0400

    Add Part 1 Exercise React part 1

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "My Books List";
+   let book1 = "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1713542603i/11297.jpg";
+   let book2 = "https://m.media-amazon.com/images/I/61XTdNzs1KL._AC_UF1000,1000_QL80_.jpg";
+   let book3 = "https://d1cm35kbp068hs.cloudfront.net/4fkdc9vc4v/thumbnail.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Norwegian Wood" />
+         <img src={book2} alt="Metro 2033" />
+         <img src={book3} alt="Fight club" />
       </div>      
    );
 }
--- part1/exercises/src/components/ChoresList.jsx
+++ part1/exercises/src/components/ChoresList.jsx
@@ -1,3 +1,26 @@
-export default function ChoresList () {
-   return <p>No content is here!</p>;
+// export default function ChoresList () {
+//    return <p>No content is here!</p>;
+// }
+import React from 'react';
+import classes from './ChoresList.module.css';
+
+
+class ChoresList extends React.Component {
+   render() {
+      const chore = ["cooking", "do laundry", "wash dishes", "take out trash", "vacuum flor"];
+      return (
+      <div>
+         <h3 className = {classes.choreHeading}>My list of chores</h3>
+         <ol>
+            <li className={classes.choresText}>{chore[0]}</li>
+            <li className={classes.choresText}>{chore[1]}</li>
+            <li className={classes.choresText}>{chore[2]}</li>
+            <li className={classes.choresText}>{chore[3]}</li>
+            <li className={classes.choresText}>{chore[4]}</li>
+         </ol>
+      </div>
+      );
+   }
 }
+
+export default ChoresList;
